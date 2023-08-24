7120b470f1afe419e9815b8d7639e7a47d13dc5f
ataztech910 <webconsult.ekb@gmail.com>
2023-08-24 21:01:42 +0200

    initial template

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+// Slightly modified from https://github.com/alexlafroscia/vite-plugin-handlebars
+import handlebars from "@glitchdotcom/vite-plugin-handlebars";
+
+// https://vitejs.dev/config/
+export default defineConfig(async ({ command, mode }) => {
+  return {
+    plugins: [
+      handlebars({
+        partialDirectory: resolve(__dirname, "layout"),
+        settingsFile: 'settings.json',
+        helpers: {
+          hostasclass: value => new URL(value).hostname.replace(/\./g, "_")
+        },
+        reloadOnPartialChange: true
+      })
+    ],
+    build: {
+      cssCodeSplit: false,
+      outDir: "build"
+    },
+    optimizeDeps: {
+      exclude: ['./settings.json']
+    },
+    server: {
+      host:"0.0.0.0",
+      port:3000,
+      strictPort: true,
+      hmr: {
+        clientPort: 443
+      }
+    }
+  };
+});
